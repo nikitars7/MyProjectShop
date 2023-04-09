91007a33f455065c8b82f8c80cd03aa46b4b4941
Nikita <nikita_rs7@ukr.net>
2023-04-09 16:42:13 +0300

    api

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,17 @@
-import products from "../myProducts";
+import { useState,useEffect } from "react";
 import Product from "./Product";
 import classNames from "classnames";
 import styles from "../styles/Products.module.scss";
 const Products = () => {
+  const [products,setProducts] = useState([]);
+  useEffect(()=>{
+    const getProducts = async() => {
+      const response = await fetch('https://6404ecfc40597b65de2d48a6.mockapi.io/Products');
+      const data = await response.json();
+      setProducts(data);
+    }
+    getProducts();
+  },[])
   return (
     <section className={styles.products}>
       <div className={styles.container}>
