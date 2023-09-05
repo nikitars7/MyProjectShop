d81230683c36a04a9f787b5b37c3131187590edd
Nikita <nikita_rs7@ukr.net>
2023-09-05 18:35:38 +0300

    adaprcartudp

--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -1,9 +1,9 @@
 import { useSelector } from "react-redux";
 import WishListItem from "../components/WishListItem";
 import styles from "../styles/WishList.module.scss";
-import '../styles/ItemsTransition.scss'
+import "../styles/ItemsTransition.scss";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
-import {useState} from 'react'
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { RootState } from "../store/store";
 import MyModal from "../UI/popup/MyModal";
@@ -11,13 +11,15 @@ import Button from "../UI/button/Button";
 const Wishlist: React.FC = () => {
   document.title = "Wishlish";
   const { wishItems } = useSelector((store: RootState) => store.wishSlice);
-  const [isVisible,setIsVisible] = useState<boolean>(false);
+  const [isVisible, setIsVisible] = useState<boolean>(false);
   return (
     <div className={styles.container}>
       <MyModal isVisible={isVisible} setIsVisible={setIsVisible}>
-        <div className={styles.wish__modal}> 
+        <div className={styles.wish__modal}>
           <h2>If you want to add this Item to cart you should be authorized</h2>
-          <Link to='/login'><Button>Authorize</Button></Link>
+          <Link to="/login">
+            <Button>Authorize</Button>
+          </Link>
         </div>
       </MyModal>
       <div className={styles.wishlist__items}>
@@ -26,8 +28,12 @@ const Wishlist: React.FC = () => {
           {wishItems && (
             <TransitionGroup>
               {wishItems.map((product) => (
-                <CSSTransition key={product.id}  timeout={500} classNames="item">
-                  <WishListItem isVisible={isVisible} setIsVisible={setIsVisible} product={product} />
+                <CSSTransition key={product.id} timeout={500} classNames="item">
+                  <WishListItem
+                    isVisible={isVisible}
+                    setIsVisible={setIsVisible}
+                    product={product}
+                  />
                 </CSSTransition>
               ))}
             </TransitionGroup>
