da9c8ed0c23800e3ef8d18bfea341d598bcc9949
Nikita <nikita_rs7@ukr.net>
2023-09-10 23:51:11 +0300

    lazyloadloader

---
+++ src/UI/loader/MyLoader.tsx
@@ -0,0 +1,22 @@
+import "./MyLoader.scss";
+import styles from "../../App.module.scss";
+const MyLoader: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <div className="loader__wrapper">
+        <div className="lds-roller">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyLoader;
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
-import { Main } from "../pages";
+import { lazy, Suspense } from "react";
 import Wishlist from "../pages/Wishlist";
 import NotFound from "../pages/NotFound";
 import Cart from "../pages/Cart";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
-import ItemsPage from "../pages/ItemsPage";
+import MyLoader from "../UI/loader/MyLoader";
+const ItemsPage = lazy(() =>
+  import(/*webpackChunkName: 'ItemsPage'*/ "../pages/ItemsPage")
+);
+const Main = lazy(() => import(/*webpackChunkName: 'Main'*/ "../pages/Main"));
 export const router = [
-  { path: "", component: <Main /> },
+  {
+    path: "",
+    component: (
+      <Suspense fallback={<MyLoader />}>
+        <Main />
+      </Suspense>
+    ),
+  },
   { path: "/wishlist", component: <Wishlist /> },
-  { path: "/items", component: <ItemsPage/> },
-  { path: "/login", component: <LoginPage/> },
+  {
+    path: "/items",
+    component: (
+      <Suspense fallback={<MyLoader />}>
+        <ItemsPage />
+      </Suspense>
+    ),
+  },
+  { path: "/login", component: <LoginPage /> },
   { path: "/register", component: <RegisterPage /> },
   { path: "/cart", component: <Cart /> },
   { path: "*", component: <NotFound /> },
