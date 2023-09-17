24b13aa229a574fb75e9e072474474790b441493
Nikita <nikita_rs7@ukr.net>
2023-09-17 22:29:41 +0300

    itempage

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,15 +1,19 @@
-import { useParams, useNavigate, Link } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { useAppDispatch } from "../store/store";
+import ProductSvgWish from "../iconsvg/ProductSvgWish";
+import { addToWishList } from "../store/slices/wishSlice";
 import axios from "axios";
 import { FetchProduct } from "../store/slices/productSlice";
-import Product from "../components/Product";
-import styles from "../App.module.scss";
+import styles from "../styles/ProductPage.module.scss";
 import MyLoader from "../UI/loader/MyLoader";
 import Button from "../UI/button/Button";
+import { CartProduct } from "../store/slices/cartSlice";
 const ProductPage: React.FC = () => {
   const [item, setItem] = useState<FetchProduct>();
   const { id } = useParams();
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
   useEffect(() => {
     const getProduct = async () => {
       try {
@@ -27,6 +31,9 @@ const ProductPage: React.FC = () => {
   const handleClickBack = () => {
     navigate(-1);
   };
+  const handleClick = () => {
+    dispatch(addToWishList(item as CartProduct));
+  };
   if (!item) {
     return (
       <div className={styles.container}>
@@ -38,11 +45,26 @@ const ProductPage: React.FC = () => {
   }
   return (
     <div className={styles.container}>
-      <div className={styles.product__content}>
-      <div className={styles.product__button} onClick={handleClickBack}>
-          <Button>Back</Button>
+      <div className={styles.item__content}>
+        <div className={styles.item__desc}>
+          <Button handleClick={handleClickBack}>Back</Button>
+          <div className={styles.item__info}>
+            <h2 className={styles.item__name}>product: {item.name}</h2>
+            <p className={styles.item__price}>price: {item.price} $</p>
+          </div>
         </div>
-        <Product product={item} />
+        <div className={styles.item__body}>
+          <div className={styles.item__image}>
+            <img src={item.imageUrl} alt={item.name} />
+            <button
+              data-tooltip="Add to WishList"
+              onClick={handleClick}
+              className={styles.add_to_wishlist}
+            >
+              <ProductSvgWish />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
